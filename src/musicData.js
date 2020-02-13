d0b0f631192f25007eb78ad7cbf2d52892ba6a8d
Lazeratrax <lazeratrax@gmail.com>
2020-02-14 01:13:54 +0300

    создал новую ветку

---
+++ src/musicData.js
@@ -0,0 +1,126 @@
+const  musicData = [
+    {
+        singer: "Slipknot",
+        song: "Before I Forget",
+        ganre: "heavy metal",
+        year: "2004"
+    },
+    {
+        singer: "Mushroomhead",
+        song: "The War Inside",
+        ganre: "alternative metal",
+        year: 2003},
+    {
+        singer: "Slipknot",
+        song: "Unsainted",
+        ganre: "heavy metal",
+        year: "2019"
+    },
+    {
+        singer: "Slipknot",
+        song: "Birth Of The Cruel",
+        ganre: "heavy metal",
+        year: "2019"
+    },
+    {
+        singer: "Slipknot",
+        song: " Death Because of Death",
+        ganre: "heavy metal",
+        year: "2019"
+    },
+    {
+        singer: "Slipknot",
+        song: " Nero Forte",
+        ganre: "heavy metal",
+        year: "2019"
+    },
+    {
+        singer: "Slipknot",
+        song: " Critical Darling",
+        ganre: "heavy metal",
+        year: "2019"
+    },
+    {
+        singer: "Slipknot",
+        song: " A Liar's Funeral",
+        ganre: "heavy metal",
+        year: "2019"
+    },
+
+    {
+        singer: "Stone Sour",
+        song: "Through Glass",
+        ganre: "hard rock",
+        year: "2006"
+    },
+    {
+        singer: "Stone Sour",
+        song: "Anna",
+        ganre: "hard rock",
+        year: "2010"
+    },
+
+    {
+        singer: "Stone Sour",
+        song: "Red City",
+        ganre: "hard rock",
+        year: "2013"
+    },
+    {
+        singer: "Stone Sour",
+        song: "Anna",
+        ganre: "hard rock",
+        year: "2010"
+
+    },
+    {
+        singer: "Stone Sour",
+        song: "Sadist",
+        ganre: "hard rock",
+        year: "2013"
+
+    },
+    {
+        singer: "Stone Sour",
+        song: "Sadist",
+        ganre: "hard rock",
+        year: "2013"
+
+    },
+    {
+        singer: "Stone Sour",
+        song: " Road Hogs",
+        ganre: "hard rock",
+        year: "2002"
+    },
+    {
+        singer: "Slipknot",
+        song: " Gematria (The Killing Name)",
+        ganre: "heavy metal",
+        year: "2006"
+
+    },
+    {
+        singer: "Slipknot",
+        song: " The Devil In I",
+        ganre: "heavy metal",
+        year: "2014"
+
+    },
+    {
+        singer: "Slipknot",
+        song: " Circle",
+        ganre: "lyric",
+        year: "2004"
+
+    },
+    {
+        singer: "Slipknot",
+        song: " Circle",
+        ganre: "lyric",
+        year: "2004"
+
+    }
+];
+
+export default musicData;
